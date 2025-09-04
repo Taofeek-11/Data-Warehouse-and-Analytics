661204767cf1af546941d72a84f73e21e452116f
oladigbolu taofeek <taoade123@gmail.com>
2025-09-04 05:12:48 +0100

    Create init_database.sql

---
+++ script/init_database.sql
@@ -0,0 +1,30 @@
+/*
+========================================================
+create database and schemas
+========================================================
+script purpose:
+	this script create a new database named 'data_warehouse' after checking if it already exists.
+    if the databse exists, it is droped and recreated. additionally, the script sets up three schemas 
+    within the database; 'bronze', 'silver', and 'gold'.
+    
+warning: 
+	running this script will drop the entire 'data_warehouse' database if it exists.
+    all data in the databse will be permanently deleted. proceed with caution and ensure you have proper 
+    backups before running this script.*/  
+
+-- drop and recreate 'data_warehouse' database 
+
+if  exists Drop database data_warehouse; 
+
+-- create database "data_warehouse" 
+Create database data_warehouse;
+use data_warehouse;
+
+-- create schemas 
+Create Schema bronze;
+
+Create Schema silver;
+
+Create Schema gold;
+
+Create Schema gold;
