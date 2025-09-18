92f42a01ad4970570a69ccacd111cc5e970e4abc
oladigbolu taofeek <taoade123@gmail.com>
2025-09-18 16:42:36 +0100

    Update ddl_bronze.sql

--- script/bronze/ddl_bronze.sql
+++ script/bronze/ddl_bronze.sql
@@ -1,14 +1,15 @@
 /*
 ======================================================================================================
-DDL Script: create bronze tables
+DDL Script: create brinze tables
 =======================================================================================================
 script purpose:
   this script creates tables in the 'bronze' schema, dropping existing tables if they already exists.
   run this script to re-define the DDL structure of 'bronze' tables
 =======================================================================================================
 */
---creating tables
-drop table if exists csv_customer;
+use data_warehouse;
+-- creating tables
+drop table if exists bronze.csv_customer;
 create table bronze.csv_customer (
 	customer_id varchar (10) primary key,
 	email varchar(100) UNIQUE,
@@ -20,11 +21,11 @@ create table bronze.csv_customer (
 	country varchar(50),
 	city varchar(50),
 	customer_segment varchar(50),
-	marketing_opt_in boolean,
+	marketing_opt_in bit,
 	preferred_category varchar(50)    
 );
 
-drop table if exists csv_products;
+drop table if exists bronze.csv_products;
 create table bronze.csv_products (
 	product_id varchar (10) primary key,
 	product_name varchar(100),
@@ -37,23 +38,24 @@ create table bronze.csv_products (
 	reorder_level int
 );
 
-drop table if exists csv_orders;
-create table bronze.csv_orders(
-order_id varchar(20) primary key,
-customer_id varchar(20),
-order_data Date,
-order_status varchar(20),
-subtotal int,
-discount_amount float,
-shipping_cost int,
-tax_amount float,
-payment_method varchar(20),
-order_source varchar(20),
-billing_country varchar(5),
-constraint fk_customer foreign key (customer_id) references customer (customer_id)
- );
+drop table if exists bronze.csv_orders;
+create table bronze.csv_orders (
+	order_id varchar (20) primary key,
+	customer_id varchar (20),
+	order_date date,
+	order_status varchar(50),
+	subtotal int,
+	discount_amount decimal (10, 2),
+	shipping_cost int,
+    tax_amount decimal (10, 2),
+	total_amount decimal (10, 2),
+	payment_method varchar(50),
+	order_source varchar(50),
+	billing_country varchar(5),
+	constraint fk_cvs_customer foreign key (customer_id) references bronze.csv_customer (customer_id)
+);
 
-drop table if exists csv_order_items;
+drop table if exists bronze.csv_order_items;
 create table bronze.csv_order_items (
 	order_item_id varchar (10) primary key,
 	order_id varchar (10),
@@ -61,32 +63,35 @@ create table bronze.csv_order_items (
 	quantity int,
 	unit_price decimal(10, 2),
 	line_total int,
-	constraint fk_orders foreign key (order_id) references csv_orders (order_id),
-	constraint fk_products foreign key (product_id) references csv_products (product_id)
+	constraint fk_orders foreign key (order_id) references bronze.csv_orders (order_id),
+	constraint fk_products foreign key (product_id) references bronze.csv_products (product_id)
 );
 
-
---inserting data into tables
+-- inserting data into tables
 delete from csv_customer;
 load data local infile 'C:\\Users\\USER\\Documents\\SQLProject\\E-commerce\\CSV_File\\customers.csv'
 into table csv_customer
 fields terminated by ','
 enclosed by '"'
-lines terminated by '\n'; 
+lines terminated by '\n'
+IGNORE 1 ROWS;  
 
 delete from csv_orders;
-load data local infile 'C:\\Users\\USER\\Documents\\SQLProject\\E-commerce\\CSV_File\\orders.csv'
-into table csv_orders
-fields terminated by '.'
-enclosed by '"'
-lines terminated by '\n';
+LOAD DATA LOCAL INFILE 'C:\\Users\\USER\\Documents\\SQLProject\\E-commerce\\CSV_File\\orders.csv' 
+INTO TABLE csv_orders 
+FIELDS TERMINATED BY ',' 
+OPTIONALLY ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'  -- Try Windows line ending
+IGNORE 1 ROWS
+(order_id, customer_id, order_date, order_status, subtotal, discount_amount, shipping_cost, total_amount, tax_amount, payment_method, order_source, billing_country);
 
 delete from csv_products;
 load data local infile 'C:\\Users\\USER\\Documents\\SQLProject\\E-commerce\\CSV_File\\products.csv'
 into table csv_products
 fields terminated by ','
 enclosed by '"'
-lines terminated by '\n';
+lines terminated by '\n'
+IGNORE 1 ROWS;
 
 delete from csv_order_items;
 load data local infile 'C:\\Users\\USER\\Documents\\SQLProject\\E-commerce\\CSV_File\\order_items.csv'
@@ -94,4 +99,4 @@ into table csv_order_items
 fields terminated by ','
 enclosed by '"'
 lines terminated by '\n'
-
+IGNORE 1 ROWS; 
